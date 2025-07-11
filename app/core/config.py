507900465db1ff50a6e79838620992dbb6aa768e
abdulazizmirjalilov <abdulaziz.mirjalilov@ventionteams.com>
2025-07-11 16:36:40 +0500

    cross allow all

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,8 @@
+import json
 import os
 from enum import Enum
 from functools import lru_cache
-from typing import Optional, Set
+from typing import Optional, Set, List
 
 from pydantic import AnyHttpUrl, PostgresDsn, RedisDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -22,7 +23,7 @@ class GlobalSettings(BaseSettings):
     DOCS_PASSWORD: str = "admin"
 
     TRUSTED_HOSTS: Set[str] = {"app", "localhost", "0.0.0.0", "127.0.0.1"}
-    BACKEND_CORS_ORIGINS: Set[AnyHttpUrl] = set()
+    BACKEND_CORS_ORIGINS: List[str] = []
 
     ENVIRONMENT: EnvironmentEnum
     DEBUG: bool = True
@@ -58,6 +59,12 @@ class GlobalSettings(BaseSettings):
             if self.DATABASE_URL
             else str(self.DATABASE_URL)
         )
+
+    @property
+    def cors_origins(self):
+        if isinstance(self.BACKEND_CORS_ORIGINS, str):
+            return json.loads(self.BACKEND_CORS_ORIGINS)
+        return self.BACKEND_CORS_ORIGINS
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", case_sensitive=True)
 
