a5cad3c6da1d16cda3a1b2a03a8be832d4eb60a4
Umarkhon Akbarov <markakbarov@gmail.com>
2025-07-14 17:12:29 +0500

    feat(cors): modify origins parameters

--- app/main.py
+++ app/main.py
@@ -105,7 +105,7 @@ async def redoc_ui(_: str = Depends(basic_http_credentials)):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["Content-Type", "Accept-Language"],
