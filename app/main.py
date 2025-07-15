744aefad4922027653bdf3d03c7b2a1b10104b4f
Umarkhon Akbarov <markakbarov@gmail.com>
2025-07-15 10:28:48 +0500

    fix(cors): add allow headers in cors middleware

--- app/main.py
+++ app/main.py
@@ -108,5 +108,6 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-    allow_headers=["Content-Type", "Accept-Language"],
+    allow_headers=["*"],
+
 )
